e153bb6989601466dee6aacae09abffcc5e5c1d8
Mati Kepa <mati@kepa.eu.org>
2024-12-04 21:23:00 +0100

    sec findings

--- new-post.sh
+++ new-post.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
 # List folders and iterate one number further
+# shellcheck disable=SC2012
 NEXT_NUM=$(ls -1 content/blog/ | awk 'END {match($0, /post([0-9]+)/, next_num); print "post" sprintf("%03d", next_num[1]+1)}')
 
 # Prompt the user for input
 echo "Please enter new post title:"
+# shellcheck disable=SC2162
 read TITLE
 
 # Exchange whitespaces to hyphens
@@ -12,10 +14,10 @@ CLEANED_TITLE=$(echo "${TITLE}" | awk '{gsub(/ /, "-"); print tolower($0)}')
 
 # Prepare folder and proper date
 NEW_POST="content/blog/${NEXT_NUM}-${CLEANED_TITLE}"
-mkdir -p ${NEW_POST}
+mkdir -p "${NEW_POST}"
 DDATE=$(date +%Y-%m-%d)
 
-cat <<_EOF >${NEW_POST}/index.md
+cat <<_EOF >"${NEW_POST}"/index.md
 ---
 title: ${TITLE}
 summary: Click for more ...
